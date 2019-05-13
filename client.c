794b7126bc0fb4c7798a86132680cd76810bf6e9
calvin <cbanning@udel.edu>
2019-05-12 20:53:19 -0400

    vscode format

--- client.c
+++ client.c
@@ -3,37 +3,35 @@
  * the file, validates it with a checksum, and exits.
  */
 
-#include <stdio.h> /*for printf() and fprintf()*/
+#include <stdio.h>      /*for printf() and fprintf()*/
 #include <sys/socket.h> /*for socket(), connect(), send(), and recv()*/
-#include <arpa/inet.h> /*for sockaddr_in and inet_addr()*/
+#include <arpa/inet.h>  /*for sockaddr_in and inet_addr()*/
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #define SEGSIZE 255 /* Maximum data segment size */
 
-
-
-void DieWithError(char *errorMessage); /* Error handling function */
-unsigned int calculateChecksum(char* path); /* Checksum calculator */
+void DieWithError(char *errorMessage);      /* Error handling function */
+unsigned int calculateChecksum(char *path); /* Checksum calculator */
 
 /* Sets fileName */
-void setFileName(char *fileName) {
+void setFileName(char *fileName)
+{
   printf("Input file name: ");
   scanf("%[^\n]%*c", fileName);
 }
 
 /* makes len indices of char* b null terminators. Used to blank strings */
-void blankBuffer(char* buffer, int len) {
-  for(int i = 0; i < len; i++)
+void blankBuffer(char *buffer, int len)
+{
+  for (int i = 0; i < len; i++)
     buffer[i] = '\0';
 }
 
-
-
 int main(int argc, char *argv[])
 {
-  int sock; /*Socket descriptor*/
+  int sock;                    /*Socket descriptor*/
   struct sockaddr_in servAddr; /* server address */
   struct sockaddr_in fromAddr; /* source address */
   unsigned short servPort;
@@ -41,20 +39,21 @@ int main(int argc, char *argv[])
   char *servIP;
   char *buffer;
   char *fileName;
-  FILE* file;
+  FILE *file;
   unsigned int servChecksum;
   unsigned int clntChecksum;
   int windowSize;
 
   /* Test for correct number of arguments */
-  if(argc != 4) {
+  if (argc != 4)
+  {
     fprintf(stderr, "Usage: %s <Server IP> <Server Port> <Window Size>\n", argv[0]);
     exit(1);
   }
 
   printf("Starting client.\n");
-  fileName = malloc(sizeof(char)*(SEGSIZE+1));
-  buffer = malloc(sizeof(char)*(SEGSIZE+1));
+  fileName = malloc(sizeof(char) * (SEGSIZE + 1));
+  buffer = malloc(sizeof(char) * (SEGSIZE + 1));
 
   /* Create a datagram/UDP socket */
   servIP = argv[1];
@@ -64,78 +63,73 @@ int main(int argc, char *argv[])
   printf("Target Port: %d\n", servPort);
   printf("Window Size: %d\n", windowSize);
 
-  if((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP))<0)
+  if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
     DieWithError("socket() failed");
 
   /*Construct the server address structure*/
-  memset(&servAddr, 0, sizeof(servAddr)); /* Zero out structure */
-  servAddr.sin_family = AF_INET;   /* Internet addr family */
-  servAddr.sin_addr.s_addr = inet_addr(servIP);   /* Server IP address */
-  servAddr.sin_port = htons(servPort);  /* Server port */
-
-
+  memset(&servAddr, 0, sizeof(servAddr));       /* Zero out structure */
+  servAddr.sin_family = AF_INET;                /* Internet addr family */
+  servAddr.sin_addr.s_addr = inet_addr(servIP); /* Server IP address */
+  servAddr.sin_port = htons(servPort);          /* Server port */
 
   /* start by asking for the filename */
   setFileName(fileName);
 
   /*send the string to the server*/
-  if(sendto(sock, fileName, strlen(fileName), 0, (struct sockaddr *)&servAddr, sizeof(servAddr)) != strlen(fileName))
+  if (sendto(sock, fileName, strlen(fileName), 0, (struct sockaddr *)&servAddr, sizeof(servAddr)) != strlen(fileName))
     DieWithError("send() sent a different number of bytes than expected");
   else
     printf("Sent request for file '%s' from %s:%d\n", fileName, servIP, servPort);
 
-
-
   /*Receive a response*/
   fromSize = sizeof(fromAddr);
-  recvfrom(sock, buffer, SEGSIZE, 0, (struct sockaddr *) &fromAddr, &fromSize);
+  recvfrom(sock, buffer, SEGSIZE, 0, (struct sockaddr *)&fromAddr, &fromSize);
 
-  if(servAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr) {
-    fprintf(stderr,"Error: received a packet from unknown source.\nCheck that the IP addresses are identical (0.0.0.0 will not match 127.0.0.1).\n");
+  if (servAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr)
+  {
+    fprintf(stderr, "Error: received a packet from unknown source.\nCheck that the IP addresses are identical (0.0.0.0 will not match 127.0.0.1).\n");
     exit(1);
   }
 
-
-
-  if(strcmp("200 - FILE FOUND", buffer) == 0) {
+  if (strcmp("200 - FILE FOUND", buffer) == 0)
+  {
     printf("Received: %s\n", buffer); /* Print the echoed arg */
 
     /* send window size to server */
     printf("Sending window size to server\n");
-    if(sendto(sock, fileName, strlen(fileName), 0, (struct sockaddr *)&servAddr, sizeof(servAddr)) != strlen(fileName))
+    if (sendto(sock, fileName, strlen(fileName), 0, (struct sockaddr *)&servAddr, sizeof(servAddr)) != strlen(fileName))
       DieWithError("send() sent a different number of bytes than expected");
     printf("Window size sent.\n");
 
     /* opening file to write, and recv and write messages into the buffer, then to the file */
     printf("Download beginning. Opening file for writing.\n");
-    file = fopen("./ClientFiles/download.txt","w");
+    file = fopen("./ClientFiles/download.txt", "w");
 
-    for(;;) {
-      blankBuffer(buffer, SEGSIZE+1);
-      recvfrom(sock, buffer, SEGSIZE, 0, (struct sockaddr *) &fromAddr, &fromSize);
-      fputs(buffer,file);
-      if(buffer[SEGSIZE - 1] == '\0') /* This segment had null bytes, EOF was reached */
+    for (;;)
+    {
+      blankBuffer(buffer, SEGSIZE + 1);
+      recvfrom(sock, buffer, SEGSIZE, 0, (struct sockaddr *)&fromAddr, &fromSize);
+      fputs(buffer, file);
+      if (buffer[SEGSIZE - 1] == '\0') /* This segment had null bytes, EOF was reached */
         break;
     }
 
     printf("Download finished. Closing file. See ./ClientFiles/download.txt\n");
     fclose(file);
     printf("Preparing to validate file with checksum.\n");
-    recvfrom(sock, &servChecksum, sizeof(unsigned int), 0, (struct sockaddr *) &fromAddr, &fromSize);
+    recvfrom(sock, &servChecksum, sizeof(unsigned int), 0, (struct sockaddr *)&fromAddr, &fromSize);
     clntChecksum = calculateChecksum("./ClientFiles/download.txt");
     printf("server checksum: %u\n", servChecksum);
     printf("client checksum: %u\n", clntChecksum);
-    if(servChecksum == clntChecksum)
+    if (servChecksum == clntChecksum)
       printf("Checksums match.\n");
     else
       printf("Checksums differ. Something went wrong.\n");
-
-
-  } else {
+  }
+  else
+  {
     printf("Received: %s\n", buffer); /* Print the echoed arg */
   }
-
-
 
   printf("Closing client.\n");
   close(sock);
--- server.c
+++ server.c
@@ -6,166 +6,164 @@
  * file.
  */
 
-#include <stdio.h> /*for printf() and fprintf()*/
+#include <stdio.h>      /*for printf() and fprintf()*/
 #include <sys/socket.h> /*for socket(), connect(), send(), and recv()*/
-#include <arpa/inet.h> /*for sockaddr_in and inet_addr()*/
+#include <arpa/inet.h>  /*for sockaddr_in and inet_addr()*/
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #define SEGSIZE 255 /* Maximum data segment size */
 
-
-
-void DieWithError(char *errorMessage); /* Error handling function */
-unsigned int calculateChecksum(char* path); /* Checksum calculator */
+void DieWithError(char *errorMessage);      /* Error handling function */
+unsigned int calculateChecksum(char *path); /* Checksum calculator */
 
 /* makes len indices of char* b null terminators. Used to blank strings */
-void blankBuffer(char* buffer, int len) {
-  for(int i = 0; i < len; i++)
-    buffer[i] = '\0';
+void blankBuffer(char *buffer, int len)
+{
+    for (int i = 0; i < len; i++)
+        buffer[i] = '\0';
 }
-
 
 /* Fills buffer b of length len with data from the filepointer file */
 /* returns 1 if EOF reached, 0 otherwise */
-int prepBuffer(FILE* file, char* buffer, int len) {
-  char next; /* next character read */
-  for(int i = 0; i < len; i++) {
-    next = fgetc(file);
-    if(next == EOF) { /* if EOF reached, null terminate and return 1 */
-      buffer[i] = '\0';
-      return 1;
-    } else { /* else continue read */
-      buffer[i] = next;
+int prepBuffer(FILE *file, char *buffer, int len)
+{
+    char next; /* next character read */
+    for (int i = 0; i < len; i++)
+    {
+        next = fgetc(file);
+        if (next == EOF)
+        { /* if EOF reached, null terminate and return 1 */
+            buffer[i] = '\0';
+            return 1;
+        }
+        else
+        { /* else continue read */
+            buffer[i] = next;
+        }
     }
-  }
-  return 0;
+    return 0;
 }
 
 /* function used to determine if a packet should have simulated biterrors */
 /* returns 1 if a packet should have simulated biterrors */
 /* returns 0 if a packet should not have simulated biterrors */
-int hasError(float p) {
-  return (p > (((float)rand()) / ((float)RAND_MAX)));
+int hasError(float p)
+{
+    return (p > (((float)rand()) / ((float)RAND_MAX)));
 }
 
 int main(int argc, char *argv[])
 {
-  int sock; /* Socket */
-  struct sockaddr_in servAddr; /* Local address */
-  struct sockaddr_in clntAddr; /* Client address */
-  unsigned int cliAddrLen; /* Length of incoming message */
-  char buffer[SEGSIZE]; /* Buffer for string */
-  char filePath[SEGSIZE]; /* Buffer for filePath */
-  unsigned short servPort; /* Server port */
-  int recvMsgSize; /* Size of received message */
-  FILE* file; /* pointer to requested file */
-  unsigned int checksum; /* sum of bytes of entire file. used for final validation  */
-  float probability; /* probability for biterror */
-  int EOFreached; /* 1 if EOF reached, 0 if not reached */
-  int windowSize; /* window size, set by sender */
+    int sock;                    /* Socket */
+    struct sockaddr_in servAddr; /* Local address */
+    struct sockaddr_in clntAddr; /* Client address */
+    unsigned int cliAddrLen;     /* Length of incoming message */
+    char buffer[SEGSIZE];        /* Buffer for string */
+    char filePath[SEGSIZE];      /* Buffer for filePath */
+    unsigned short servPort;     /* Server port */
+    int recvMsgSize;             /* Size of received message */
+    FILE *file;                  /* pointer to requested file */
+    unsigned int checksum;       /* sum of bytes of entire file. used for final validation  */
+    float probability;           /* probability for biterror */
+    int EOFreached;              /* 1 if EOF reached, 0 if not reached */
+    int windowSize;              /* window size, set by sender */
 
-  /* Test for correct number of arguments */
-  if(argc != 3) {
-    fprintf(stderr, "Usage: %s <Server Port> <Biterror Probability>\n", argv[0]) ;
-    exit(1);
-  }
-
-
-
-  printf("Starting server.\n");
-  servPort = atoi(argv[1]); /* First arg: local port */
-  probability = atof(argv[2]); /* Second arg: error probability */
-  printf("Server Port: %d\n", servPort);
-  printf("Bit Error Probability: %f\n", probability);
-
-
-  /* Create socket for incoming connections */
-  if((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
-    DieWithError("socket() failed");
-
-  /* Construct local address structure */
-  memset(&servAddr, 0, sizeof(servAddr)); /* Zero out structure */
-  servAddr.sin_family = AF_INET; /* Internet address family */
-  servAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
-  servAddr.sin_port = htons(servPort); /* Local port */
-
-  /* Bind to the local address */
-  if (bind(sock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
-    DieWithError ( "bind() failed");
-
-  /* Set the size of the in-out parameter */
-  // Note to self: this was originally in the for loop, does it belong there?
-  cliAddrLen = sizeof(clntAddr);
-
-
-
-  for (;;) { /* run until break */
-    /* blank the buffers */
-    blankBuffer(buffer, SEGSIZE);
-    blankBuffer(filePath, SEGSIZE);
-
-    strcpy(filePath,"./ServerFiles/");
-
-
-
-    /* Block until receive message from a client */
-    if((recvMsgSize = recvfrom(sock, buffer, SEGSIZE, 0, (struct sockaddr *) &clntAddr, &cliAddrLen)) < 0)
-      DieWithError("recvfrom() failed") ;
-
-    printf("Handling client %s\n", inet_ntoa(clntAddr.sin_addr));
-
-
-    /* Check that the file exists and is readable */
-    if(access(strcat(filePath,buffer), R_OK) != -1) {
-      /* The file exists and is readable */
-      printf("Requested file '%s' exists.\n", filePath);
-      /* Send OK to the client */
-      if(sendto(sock, "200 - FILE FOUND", strlen("200 - FILE FOUND"), 0, (struct sockaddr *) &clntAddr, sizeof(clntAddr)) != strlen("200 - FILE FOUND"))
-        DieWithError("sendto() sent a different number of bytes than expected");
-      else { /* 200 sent to client. Now send the file */
-        file = fopen(filePath, "r");
-        printf("'%s' opened. Preparing to buffer and send.\n", filePath);
-
-
-
-        /* File transfer until break*/
-        EOFreached = 0;
-        while(!EOFreached)
-          {
-            blankBuffer(buffer, SEGSIZE);
-
-            /* EOF reached. Send and break */
-            if(prepBuffer(file, buffer, SEGSIZE)) {
-              sendto(sock, buffer, SEGSIZE, 0, (struct sockaddr *) &clntAddr, sizeof(clntAddr));
-              EOFreached = 1;
-            } else { /* send data as normal */
-              sendto(sock, buffer, SEGSIZE, 0, (struct sockaddr *) &clntAddr, sizeof(clntAddr));
-            }
-          }
-
-
-
-        fclose(file);
-
-        printf("Getting ready to send checksum\n");
-        checksum = calculateChecksum(filePath);
-        sendto(sock, &checksum, sizeof(unsigned int), 0, (struct sockaddr *) &clntAddr, sizeof(clntAddr));
-
-      }
-    } else {
-      /* The file does not exist or is not readable */
-      printf("Requested file '%s' does not exist.\n", filePath);
-      if(sendto(sock, "404 - FILE NOT FOUND", strlen("404 - FILE NOT FOUND"), 0, (struct sockaddr *) &clntAddr, sizeof(clntAddr)) != strlen("404 - FILE NOT FOUND"))
-        DieWithError("sendto() sent a different number of bytes than expected");
+    /* Test for correct number of arguments */
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: %s <Server Port> <Biterror Probability>\n", argv[0]);
+        exit(1);
     }
-  }
 
+    printf("Starting server.\n");
+    servPort = atoi(argv[1]);    /* First arg: local port */
+    probability = atof(argv[2]); /* Second arg: error probability */
+    printf("Server Port: %d\n", servPort);
+    printf("Bit Error Probability: %f\n", probability);
 
+    /* Create socket for incoming connections */
+    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+        DieWithError("socket() failed");
 
-  printf("Closing socket.\n");
-  close(sock);
-  printf("Shutting down server.\n");
-  exit(0);
+    /* Construct local address structure */
+    memset(&servAddr, 0, sizeof(servAddr));       /* Zero out structure */
+    servAddr.sin_family = AF_INET;                /* Internet address family */
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
+    servAddr.sin_port = htons(servPort);          /* Local port */
+
+    /* Bind to the local address */
+    if (bind(sock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
+        DieWithError("bind() failed");
+
+    /* Set the size of the in-out parameter */
+    // Note to self: this was originally in the for loop, does it belong there?
+    cliAddrLen = sizeof(clntAddr);
+
+    for (;;)
+    { /* run until break */
+        /* blank the buffers */
+        blankBuffer(buffer, SEGSIZE);
+        blankBuffer(filePath, SEGSIZE);
+
+        strcpy(filePath, "./ServerFiles/");
+
+        /* Block until receive message from a client */
+        if ((recvMsgSize = recvfrom(sock, buffer, SEGSIZE, 0, (struct sockaddr *)&clntAddr, &cliAddrLen)) < 0)
+            DieWithError("recvfrom() failed");
+
+        printf("Handling client %s\n", inet_ntoa(clntAddr.sin_addr));
+
+        /* Check that the file exists and is readable */
+        if (access(strcat(filePath, buffer), R_OK) != -1)
+        {
+            /* The file exists and is readable */
+            printf("Requested file '%s' exists.\n", filePath);
+            /* Send OK to the client */
+            if (sendto(sock, "200 - FILE FOUND", strlen("200 - FILE FOUND"), 0, (struct sockaddr *)&clntAddr, sizeof(clntAddr)) != strlen("200 - FILE FOUND"))
+                DieWithError("sendto() sent a different number of bytes than expected");
+            else
+            { /* 200 sent to client. Now send the file */
+                file = fopen(filePath, "r");
+                printf("'%s' opened. Preparing to buffer and send.\n", filePath);
+
+                /* File transfer until break*/
+                EOFreached = 0;
+                while (!EOFreached)
+                {
+                    blankBuffer(buffer, SEGSIZE);
+
+                    /* EOF reached. Send and break */
+                    if (prepBuffer(file, buffer, SEGSIZE))
+                    {
+                        sendto(sock, buffer, SEGSIZE, 0, (struct sockaddr *)&clntAddr, sizeof(clntAddr));
+                        EOFreached = 1;
+                    }
+                    else
+                    { /* send data as normal */
+                        sendto(sock, buffer, SEGSIZE, 0, (struct sockaddr *)&clntAddr, sizeof(clntAddr));
+                    }
+                }
+
+                fclose(file);
+
+                printf("Getting ready to send checksum\n");
+                checksum = calculateChecksum(filePath);
+                sendto(sock, &checksum, sizeof(unsigned int), 0, (struct sockaddr *)&clntAddr, sizeof(clntAddr));
+            }
+        }
+        else
+        {
+            /* The file does not exist or is not readable */
+            printf("Requested file '%s' does not exist.\n", filePath);
+            if (sendto(sock, "404 - FILE NOT FOUND", strlen("404 - FILE NOT FOUND"), 0, (struct sockaddr *)&clntAddr, sizeof(clntAddr)) != strlen("404 - FILE NOT FOUND"))
+                DieWithError("sendto() sent a different number of bytes than expected");
+        }
+    }
+
+    printf("Closing socket.\n");
+    close(sock);
+    printf("Shutting down server.\n");
+    exit(0);
 }
